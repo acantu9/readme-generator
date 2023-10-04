78d290fe5158234e1f14b2ec762760e9e9e1cdc4
acantu9 <adrianaacantu@gmail.com>
2023-10-04 00:10:19 -0500

    added validation functions to questions that requires user input and prompts to receive said input

--- Develop/index.js
+++ Develop/index.js
@@ -6,49 +6,102 @@ const questions = [
     {
         type: 'input',
         name: 'project',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project?',
+        validate: function (input) {
+            if (input.trim() === '') {
+              return 'Please enter the name of your project.';
+            }
+            return true;
+        }
     },
     {
         type: 'input',
         name: 'description',
-        message: 'What is the description of your project?'
+        message: 'What is the description of your project?',
+        validate: function (input) {
+            if (input.trim() === '') {
+              return 'Please enter the description of your project.';
+            }
+            return true;
+          }
     },
     {
         type: 'input',
         name: 'installation',
-        message: 'What are the installation instructions of your project?'
+        message: 'What are the installation instructions of your project?',
+        validate: function (input) {
+            if (input.trim() === '') {
+              return 'Please enter the installation instructions of your project.';
+            }
+            return true;
+          }
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'How do you use your project?'
+        message: 'How do you use your project?',
+        validate: function (input) {
+            if (input.trim() === '') {
+              return 'Please describe how to use your project.';
+            }
+            return true;
+          }
     },
     {
         type: 'input',
         name: 'contribution',
-        message: 'What are the contribution guidelines for your project?'
+        message: 'What are the contribution guidelines for your project?',
+        validate: function (input) {
+            if (input.trim() === '') {
+              return 'Please enter the contribution guidelines for your project.';
+            }
+            return true;
+          }
     },
     {
         type: 'input',
         name: 'test',
-        message: 'How do you test your project?'
+        message: 'How do you test your project?',
+        validate: function (input) {
+            if (input.trim() === '') {
+              return 'Please enter the instructions to test your project.';
+            }
+            return true;
+          }
     },
     {
         type: 'list',
         name: 'license',
         message: 'What type of license would you like to use for your project?',
-        choices: ['MIT License', 'BSD License', 'GPL', 'AGPL'],
+        choices: ['MIT License', 'BSD License', 'GPL', 'AGPL']
     },
     {
         type: 'input',
         name: 'username',
-        message: 'What is your GitHub username?'
+        message: 'What is your GitHub username?',
+        validate: function (input) {
+            if (input.trim() === '') {
+              return 'Please enter your GitHub username.';
+            }
+            return true;
+          }
     },
     {
         type: 'input',
         name: 'email',
-        message: 'What is your email address?'
+        message: 'What is your email address?',
+        validate: function (input) {
+            if (input.trim() === '') {
+              return 'Please enter your email address.';
+            }
+            return true;
+          }
     },
+
+    // Promts user to answer questions
+    inquirer.prompt(questions).then((answers) => {
+        console.log('User answers:', answers.trim());
+    })
 ];
 
 // TODO: Create a function to write README file
