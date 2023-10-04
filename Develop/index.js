91bc35167b7a055a8773b218e3d289285e817508
acantu9 <adrianaacantu@gmail.com>
2023-10-03 23:44:22 -0500

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,55 @@
-// TODO: Include packages needed for this application
+// Include inquirer package
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'project',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions of your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines for your project?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How do you test your project?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What type of license would you like to use for your project?',
+        choices: ['MIT License', 'BSD License', 'GPL', 'AGPL'],
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
