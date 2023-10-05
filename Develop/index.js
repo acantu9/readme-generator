a02cbc44ad3dafdcd4431195593fff2904a37fde
acantu9 <adrianaacantu@gmail.com>
2023-10-04 19:49:40 -0500

    defined fileName & data, created writeToFile function

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,48 @@
 // Include inquirer package
 const inquirer = require('inquirer');
 const fs = require('fs');
+const fileName = 'README.md';
+
+const data = ({ project, description, installation, usage, contribution, test, license, username, email }) => {
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <title>README Generator</title>
+    </head>
+    <body>
+    <header class="p-5 mb-4 header bg-light">
+    <div class="container">
+        <h1 id="project" class="display-4">${project.trim()}</h1>
+        <h2 id="description">Description</h2>
+        <p class="lead">${description.trim()}.</p>
+        <h2 id="tableOfCont">Table of Contents</h2>
+        <a href="#installation">Installation</a>
+        <a href="#usage">Usage</a>
+        <a href="#contribution">Contribution</a>
+        <a href="#test">Tests</a>
+        <a href="#license">License</a>
+        <a href="#questions">Questions</a>
+        <h2 id="installation">Installation</h2>
+        <p class="lead">${installation.trim()}.</p>
+        <h2 id"usage">Usage</h2>
+        <p class="lead">${usage.trim()}.</p>
+        <h2 id="contribution">Contributing</h2>
+        <p class="lead">${contribution.trim()}.</p>
+        <h2 id="test">Tests</h2>
+        <p class="lead">${test.trim()}.</p>
+        <h2 id="license">License</h2>
+        <p class="lead">${license}.</p>
+        <h2 id="questions">Questions/h2>
+        <p class="lead">Questions? Email me: ${email.trim()}</p>
+        <p class="lead">GitHub username: ${username.trim()}</p>
+    </div>
+    </header>
+    </body>
+    </html>`;
+}
 
 // Array of questions for user input
 const questions = [
@@ -106,7 +148,15 @@ inquirer.prompt(questions).then((answers) => {
 })
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('README file created successfully!');
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
