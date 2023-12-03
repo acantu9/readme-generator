18c40c95ad30d405d6fd23cb38d462c073c1e52d
acantu9 <adrianaacantu@gmail.com>
2023-12-02 20:04:09 -0600

    added code to all the functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,84 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    // Return the license badge based on the license value
+    return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)](https://opensource.org/licenses/${license})`;
+  } else {
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    // Return the license link based on the license value
+    // For example, you can use Markdown to generate a link
+    return `[License](https://opensource.org/licenses/${license})`;
+  } else {
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    // Return the license section based on the license value
+    // For example, you can use Markdown to generate a section
+    return `## License
 
-// TODO: Create a function to generate markdown for README
+      This project is licensed under the [${license}](https://opensource.org/licenses/${license}) license.`;
+  } else {
+    return '';
+  }
+}
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  For any questions, please contact me:
+
+  - GitHub: [${data.github}](https://github.com/${data.github})
+  - Email: ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
