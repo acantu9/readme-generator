a88639e82447610610053b77d618365fc70fb9a0
acantu9 <adrianaacantu@gmail.com>
2023-12-02 23:02:11 -0600

    refractored generateMarkdown for readability and added testREADME2.md to .gitignore

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,40 +1,39 @@
 // Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license) {
-    // Return the license badge based on the license value
-    return `![License](https://img.shields.io/badge/License-${data.license}-blue.svg)(https://opensource.org/licenses/${data.license})`;
+function renderLicenseBadge(data) {
+  if (!data.license) {
+    return ``;
   } else {
-    return '';
+    // Return the license badge based on the license value
+    return '![License](https://img.shields.io/badge/License-${data.license}-blue.svg)(https://opensource.org/licenses/${data.license})';
   }
 }
  
 // Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license) {
-    // Return the license link based on the license value
-    return `[License](https://opensource.org/licenses/${data.license})`;
+function renderLicenseLink(data) {
+  if (!data.license) {
+    return ``;
   } else {
-    return '';
+    // Return the license link based on the license value
+    return '[License](https://opensource.org/licenses/${data.license})';
   }
 }
  
 // Function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license) {
-    // Return the license section based on the license value
-    return `## License
-      This project is licensed under the [${data.license}](https://opensource.org/licenses/${data.license}) license.`;
+function renderLicenseSection(data) {
+  if (!data.license) {
+    return ``;
   } else {
-    return '';
+    // Return the license section based on the license value
+    return '## License<br><br>This project is licensed under the [${data.license}](https://opensource.org/licenses/${data.license}).';
   }
 }
  
 // Function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.project}
+  return `## ${data.project}
  
   ${renderLicenseBadge(data.license)}
  
@@ -58,7 +57,10 @@ function generateMarkdown(data) {
   ## Usage
  
   ${data.usage}
- 
+  
+  ## License
+
+  This project is licensed under the ${data.license}.
   ${renderLicenseSection(data.license)}
   ${renderLicenseLink(data.license)}
  
